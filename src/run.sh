46039fe21bc5985e2781d617b003ebbfc91f90da
zhongpanwu <symwanyy@gmail.com>
2023-03-17 18:20:29 -0400

    init commit

---
+++ src/run.sh
@@ -0,0 +1,5 @@
+ #!/bin/bash
+ 
+python targetcall.py ../Klebsiella_pneumoniae_KSB1_7F/new/ ../Klebsiella_pneumoniae_KSB1_7F/.fasta TINYX011 ../Klebsiella_pneumoniae_KSB1_7F/results/
+
+#python targetcall.py ../sample_data/fast5/ ../sample_data/Monkeypox_virus.fasta TINYX011 ../sample_data/
